18549767f5599506eee75258508718c9ba31ca5a
rakeshls <73830663+rakeshls@users.noreply.github.com>
2022-04-16 17:05:08 +0530

    Add files via upload

---
+++ js/markerHandler.js
@@ -0,0 +1,92 @@
+AFRAME.registerComponent("markerhandler", {
+  init: async function () {
+
+    //get the dishes collection from firestore database
+    var dishes = await this.getDishes();
+
+    //markerFound event
+    this.el.addEventListener("markerFound", () => {
+      var markerId = this.el.id;      
+      this.handleMarkerFound(dishes, markerId);
+    });
+
+    //markerLost event
+    this.el.addEventListener("markerLost", () => {
+      this.handleMarkerLost();
+    });
+
+  },
+  handleMarkerFound: function (dishes, markerId) {
+
+    var todaysDate = new Date();
+      var todaysDay = todaysDate.getDay();
+      // Sunday - Saturday : 0 - 6
+      var days = [
+        "sunday",
+        "monday",
+        "tuesday",
+        "wednesday",
+        "thursday",
+        "friday",
+        "saturday"
+      ];
+    // Changing button div visibility
+    var buttonDiv = document.getElementById("button-div");
+    buttonDiv.style.display = "flex";
+
+    var ratingButton = document.getElementById("rating-button");
+    var orderButtton = document.getElementById("order-button");
+
+    // Handling Click Events
+    ratingButton.addEventListener("click", function () {
+      swal({
+        icon: "warning",
+        title: "Rate Dish",
+        text: "Work In Progress"
+      });
+    });
+
+    orderButtton.addEventListener("click", () => {
+      swal({
+        icon: "https://i.imgur.com/4NZ6uLY.jpg",
+        title: "Thanks For Order !",
+        text: "Your order will serve soon on your table!"
+      });
+    });
+
+    // Changing Model scale to initial scale
+    var dish = dishes.filter(dish => dish.Id === markerId)[0];
+     
+    if(dish.unavailable_days.includes(days[todaysDate])){
+      swal({
+        icon:'warning',
+        title:dish.Dish_Name.toUppercase(),
+        text:"Sorry! not available today",
+        time:2500,
+        button:false
+      })
+    }
+    else{}
+
+    var model = document.querySelector(`#model-${dish.Id}`);
+    model.setAttribute("position", dish.model_geometry.position);
+    model.setAttribute("rotation", dish.model_geometry.rotation);
+    model.setAttribute("scale", dish.model_geometry.scale);
+  },
+
+  handleMarkerLost: function () {
+    // Changing button div visibility
+    var buttonDiv = document.getElementById("button-div");
+    buttonDiv.style.display = "none";
+  },
+  //get the dishes collection from firestore database
+  getDishes: async function () {
+    return await firebase
+      .firestore()
+      .collection("Dishes")
+      .get()
+      .then(snap => {
+        return snap.docs.map(doc => doc.data());
+      });
+  }
+});
